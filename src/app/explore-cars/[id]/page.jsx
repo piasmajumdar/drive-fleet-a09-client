f0f3d219a94718be28d4a8bcd2c1a50744ebf9d2
piasmajumdar <piasmajumdar@gmail.com>
2026-05-21 02:45:11 +0530

    Single Car data loaded from backend

--- src/app/explore-cars/[id]/page.jsx
+++ src/app/explore-cars/[id]/page.jsx
@@ -1,7 +1,13 @@
 const CarDetailsPage = async({params}) => {
-    const {id} = await params
+    const {id} = await params;
+
+    const res = await fetch(`${process.env.BACKEND_SERVER_URL}/cars/${id}`);
+    const car = await res.json();
+    console.log(car)
+
+    
     return (
-        <div>
+        <div className="w-11/12 mx-auto">
             Car Details
         </div>
     );
