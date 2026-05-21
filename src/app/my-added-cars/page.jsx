eaecc3da277d07fc494820e12d40db3cd8f26a34
piasmajumdar <piasmajumdar@gmail.com>
2026-05-21 13:42:44 +0530

    My added cars: fetched the added cars

--- src/app/my-added-cars/page.jsx
+++ src/app/my-added-cars/page.jsx
@@ -1,8 +1,36 @@
+import AddedCarCard from "@/components/AddedCarCard";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-const MyAddedCar = () => {
+const MyAddedCar = async () => {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    const ownerId = session?.user?.id;
+    console.log(ownerId)
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/cars/my-added-cars/${ownerId}`);
+    const myAddedCars = await res.json();
+
     return (
-        <div>
-            My Added Car
+        <div className="min-h-screen bg-gray-50 py-10">
+            <div className="w-11/12 mx-auto">
+                {/* Header */}
+                <div className="mb-8">
+                    <h1 className="text-3xl font-bold text-gray-900">My Added Cars</h1>
+                    <p className="mt-2 text-gray-500">
+                        Manage the car you have listed
+                    </p>
+                </div>
+
+                {/* Added Car */}
+                <div className="grid grid-cols-1 lg:grid-cols-4 sm:grid-cols-3 gap-3 my-3">
+                    {
+                        myAddedCars.map(myAddedCar => <AddedCarCard key={myAddedCar._id} car={myAddedCar}></AddedCarCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/AddedCarCard.jsx
@@ -0,0 +1,67 @@
+import { Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+const AddedCarCard = ({car}) => {
+    return (
+        <div className="w-full h-full flex flex-col rounded-2xl border border-gray-200 p-3 shadow-sm hover:shadow-md hover:shadow-red-300 hover:scale-105 transition-all duration-300">
+        
+                    {/* Image Section */}
+                    <div className="relative overflow-hidden rounded-xl">
+                        <Image
+                            src={car?.imageURL?.[0] &&
+                                car.imageURL[0].startsWith("http")
+                                ? car.imageURL[0]
+                                : "/fallback-image.png"}
+                            alt="Toyota RAV4"
+                            height={300}
+                            width={300}
+                            className="h-[200px] w-full object-cover"
+                        />
+                        {/* Available Badge */}
+                        <button className="absolute right-7 top-3 flex h-10 w-10 items-center justify-center shadow-md">
+                            <div className={`p-2 rounded-md ${car.availabilityStatus === "Available" ? "bg-green-200" : "bg-gray-400"}`}>
+                                <p>{car?.availabilityStatus}</p>
+                            </div>
+                        </button>
+                    </div>
+        
+                    {/* Content */}
+                    <div className="mt-4  flex flex-col flex-1">
+                        <h2 className="text-2xl font-bold dark:text-red-600 text-gray-900">
+                            {car?.carName}
+                        </h2>
+        
+                        {/* Specs */}
+                        <div className="mt-2 flex items-center gap-3 text-sm text-gray-500">
+                            <span>{car?.carType}</span>
+                            <span>•</span>
+                            <span>{car?.pickupLocation} Seats</span>
+                        </div>
+        
+                        {/* Location */}
+                        <div className="flex items-center gap-2 text-gray-500 mt-2">
+                            <FaMapMarkerAlt className="text-sm" />
+                            <span className="text-sm font-medium">{car?.pickupLocation}</span>
+                        </div>
+        
+                        {/* Bottom Row */}
+                        <div className="mt-auto flex items-center justify-end flex-1">
+                            {/* Price */}
+                            <div className="flex items-end gap-1">
+                                <span className="text-xl font-bold text-red-500">
+                                    BDT{car?.dailyRentPrice}
+                                </span>
+                                <span className="mb-1 text-sm text-gray-500">
+                                    /day
+                                </span>
+                            </div>
+                        </div>
+                        <Link href={`/explore-cars/${car._id}`}><Button variant="danger" className={'bg-[#c40101] mt-auto'}>View Details</Button></Link>
+                    </div>
+                </div>
+    );
+};
+
+export default AddedCarCard;
