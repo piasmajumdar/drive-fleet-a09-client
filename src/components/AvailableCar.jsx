49e1e7ec85f0280dce2f8b326f1313d192fd2c7b
piasmajumdar <piasmajumdar@gmail.com>
2026-05-22 17:09:26 +0530

    Added view-all button in the feature-available-car header

--- src/components/AvailableCar.jsx
+++ src/components/AvailableCar.jsx
@@ -1,5 +1,7 @@
 import { plusJakarta } from "@/app/layout";
 import AvailableCarScroller from "./AvailableCarScroller";
+import { Button } from "@heroui/react";
+import Link from "next/link";
 
 const AvailableCar = async () => {
 
@@ -11,7 +13,10 @@ const AvailableCar = async () => {
 
     return (
         <div className="w-11/12 mx-auto py-4">
-            <h2 className={`text-3xl mb-4 font-bold ${plusJakarta.className}`}>Popular Available <span className="text-red-700">Cars</span></h2>
+            <div className="flex justify-between items-center gap-1">
+                <h2 className={`text-3xl mb-4 font-bold ${plusJakarta.className}`}>Popular Available <span className="text-red-700">Cars</span></h2>
+                <Link href={'/explore-cars'}><Button variant="outline" className={'text-red-700 rounded-md sm:text-xl p-1 sm:p-4'}>View All</Button></Link>
+            </div>
             <AvailableCarScroller cars={cars}></AvailableCarScroller>
         </div>
     );
